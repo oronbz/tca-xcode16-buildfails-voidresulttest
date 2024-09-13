0e6951f24d201d3c1aebf5faf40b5f316e3c17d6
Oron Ben Zvi <oronbz@gmail.com>
2024-09-13 10:25:23 +0300

    Initial Commit

---
+++ ResultTestFail/ResultTestFailApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTestFailApp.swift
+//  ResultTestFail
+//
+//  Created by Oron Ben Zvi on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ResultTestFailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
